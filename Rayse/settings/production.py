22cab3a5842e9e7ae30763362a90702a52983df4
Aniloid2 <brianformento@hotmail.co.uk>
2017-02-20 23:49:47 +0000

    s3 works localy, all statics in cloud, need heroku test add keys

--- Rayse/settings/production.py
+++ Rayse/settings/production.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 
 AUTHENTICATION_BACKENDS = (
@@ -153,19 +153,21 @@ AUTHENTICATION_BACKENDS = (
 AUTH_USER_MODEL = 'authentication.MyUser'
 
 
-#dificulties to figure out after clicking one, small 2/3 page tutorial at the start  
-#everyones level showld try and aproach one, from 100? 
-#level at 1? higest level. 
-#first number as one, so you can find the one...
 
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
 
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-AWS_ACCESS_KEY_ID = "AKIAJPJMTZBRA442TQNA"
-
-AWS_SECRET_ACCESS_KEY = "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"
-
-AWS_STORAGE_BUCKET_NAME= "s3rayse"
--- Rayse/urls.py
+++ Rayse/urls.py
@@ -48,5 +48,6 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# ## local static ## 
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
